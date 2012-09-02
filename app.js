64d35eecff512fda7b4ab1c83fc213e3e09dd5cc
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2012-09-03 04:08:33 +0900

    Refactoring.

--- app.js
+++ app.js
@@ -2,19 +2,20 @@
  * Module dependencies.
  */
 var express = require('express')
-  //, routes = require('./routes')
+  , routes = require('./routes/main')
   , http = require('http')
   , path = require('path')
   , md = require("github-flavored-markdown").parse
   , fs = require("fs")
   , moment = require("moment");
 
-var app = express();
+var app = module.exports = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('entries', __dirname + '/entries');
+  app.set('main_title', 'org-node');
   app.engine('.md', require('ejs').__express);
   app.set('view engine', 'md');
   app.use(express.favicon());
@@ -29,70 +30,22 @@ app.configure(function(){
 });
 
 app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
 app.get('/', function(req, res, next) {
-      res.render('index.ejs', {title: 'Markdown', script: 'index.js'});
+  res.render('index.ejs', {main_title: app.get('main_title'),
+                           title: 'main',
+                           script: 'index.js'});
 });
 
-app.get('/entries', function(req, res, next) {
-    moment().local();
-    fs.readdir(app.get('entries'), function(err, files) {
-        if (err) {
-            next(err);
-            res.send('readdir error', 500);
-            return;
-        }
-        if (!files) {
-            res.send('Can\'t read entries', 404);
-            return;
-        }
-        if (files.length < 1) {
-            res.send('Can\'t read entries', 404);
-            return;
-        }
-        var entries = new Array(files.length);
-        for (var i = 0; i < files.length; i++) {
-            var stats = fs.statSync(app.get('entries') + '/' + files[i]);
-            var m = moment(stats.ctime);
-            entries[i] = new Entry(files[i], m.unix());
-        }
-        entries.sort(function(a, b) {
-            return b.ctime - a.ctime;
-        });
-        res.type('json');
-        res.json(entries);
-    });
-});
+app.get('/entries', routes.entries); 
 
-app.get('/entry/:content', function(req, res) {
-    var path = app.get('entries') + '/' + req.params.content;
-    if (!path.match(/.md$/)) {
-        path += '.md';
-    }
-    fs.readFile(path, 'utf8', function(err, data) {
-        if (err) {
-            res.send('Can\'t read entries', 404);
-            return;
-        }
-        fs.stat(path, function(err, stats) {
-            if (err) throw err;
-            var entry = new Entry(req.params.content, stats.ctime, data);
-            if (req.xhr) {
-                res.type('json');
-                var result = {};
-                result.name = entry.name;
-                result.html = entry.html();
-                result.datetime = entry.datetime();
-                result.pubdate = entry.pubdate();
-                res.json(result);
-            } else {
-                res.render('entry.ejs', {title: entry.title(), content: entry.html(), datetime: entry.datetime(), pubdate: entry.pubdate(), script: 'entry.js'});
-            }
-        });
-    });
-});
+app.get('/entry/:content', routes.entry);
 
 app.get('/fail', function(req, res) {
     res.render('missing');
@@ -102,38 +55,3 @@ http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
 
-function Entry(name, ctime, source) {
-    var parsed = false;
-    var content = null;
-    moment().local();
-    this.m = moment(ctime);
-    this.name = name;
-    this.ctime = ctime;
-    this.source = source;
-    this.title = function() {
-        if (source) {
-            if (!parsed) {
-                content = this.html();
-                var title = content.match(/<h1>([^<]*)<\/h1>/i);
-                return title[1];
-            }
-        }
-        return null;
-    };
-    this.unix = function() {
-        return m.unix();
-    };
-    this.html = function() {
-        if (source && !parsed) {
-            content = md(this.source);
-            parsed = true;
-        }
-        return content;
-    };
-    this.pubdate = function() {
-        return this.m.format('YYYY/M/D hh:mm:ss ZZ');
-    };
-    this.datetime = function() {
-        return this.m.format();
-    };
-}
---
+++ routes/main.js
@@ -0,0 +1,112 @@
+var path = require('path')
+  , md = require("github-flavored-markdown").parse
+  , fs = require("fs")
+  , moment = require("moment");
+
+
+/**
+ * get entries.
+ */
+exports.entries = function(req, res, next) {
+    moment().local();
+    fs.readdir(module.parent.exports.get('entries'), function(err, files) {
+        if (err) {
+            next(err);
+            res.send('readdir error', 500);
+            return;
+        }
+        if (!files) {
+            res.send('Can\'t read entries', 404);
+            return;
+        }
+        if (files.length < 1) {
+            res.send('Can\'t read entries', 404);
+            return;
+        }
+        var entries = new Array(files.length);
+        for (var i = 0; i < files.length; i++) {
+            var stats = fs.statSync(module.parent.exports.get('entries') + '/' + files[i]);
+            var m = moment(stats.ctime);
+            entries[i] = new Entry(files[i], m.unix());
+        }
+        entries.sort(function(a, b) {
+            return b.ctime - a.ctime;
+        });
+        res.type('json');
+        res.json(entries);
+    });
+};
+
+/**
+ * get entry by filename.
+ */
+
+exports.entry = function(req, res) {
+        var path = module.parent.exports.get('entries') + '/' + req.params.content;
+    if (!path.match(/.md$/)) {
+        path += '.md';
+    }
+    fs.readFile(path, 'utf8', function(err, data) {
+        if (err) {
+            res.send('Can\'t read entries', 404);
+            return;
+        }
+        fs.stat(path, function(err, stats) {
+            if (err) throw err;
+            var entry = new Entry(req.params.content, stats.ctime, data);
+            if (req.xhr) {
+                res.type('json');
+                var result = {};
+                result.name = entry.name;
+                result.html = entry.html();
+                result.datetime = entry.datetime();
+                result.pubdate = entry.pubdate();
+                res.json(result);
+            } else {
+                res.render('entry.ejs', {
+                               main_title: module.parent.exports.get('main_title'),
+                               title: entry.title(),
+                               content: entry.html(),
+                               datetime: entry.datetime(),
+                               pubdate: entry.pubdate(),
+                               script: 'entry.js'});
+            }
+        });
+    });
+};
+
+function Entry(name, ctime, source) {
+    var parsed = false;
+    var content = null;
+    moment().local();
+    this.m = moment(ctime);
+    this.name = name;
+    this.ctime = ctime;
+    this.source = source;
+    this.title = function() {
+        if (source) {
+            if (!parsed) {
+                content = this.html();
+                var title = content.match(/<h1>([^<]*)<\/h1>/i);
+                return title[1];
+            }
+        }
+        return null;
+    };
+    this.unix = function() {
+        return m.unix();
+    };
+    this.html = function() {
+        if (source && !parsed) {
+            content = md(this.source);
+            parsed = true;
+        }
+        return content;
+    };
+    this.pubdate = function() {
+        return this.m.format('YYYY/M/D hh:mm:ss ZZ');
+    };
+    this.datetime = function() {
+        return this.m.format();
+    };
+}
